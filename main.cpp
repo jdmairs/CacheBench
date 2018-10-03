8ff7a9c4497c33075dcb9cd21a5b1556394b0913
John Mairs <John.D.Mairs@gmail.com>
2018-10-02 21:31:24 -0400

    actual running cachebench code albeit crappy

--- main.cpp
+++ main.cpp
@@ -2,31 +2,33 @@
 #include "RNG.h"
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-	for (auto _ : state)
-		std::string empty_string;
+static void cacheBench(benchmark::State& state) {
+	int bytes = 1 << state.range(0);
+	// compute how many elements we can fit into this many bytes given that we
+	// need 2x to represent both data and indices into the data
+
+	int count = (bytes / sizeof(int)) / 2;
+	std::vector<int> v;
+	for (auto i : RNG<int>(std::numeric_limits<int>::min(),
+		std::numeric_limits<int>::max(), count))
+		v.push_back(i);
+	std::vector<int> indices;
+	for (auto i : RNG<int>(0, count, count))
+		indices.push_back(i);
+
+	while (state.KeepRunning())
+	{
+		long sum = 0;
+		for (int i : indices)
+			sum += v[i];
+		benchmark::DoNotOptimize(sum);
+	}
+
+	state.SetBytesProcessed(long(state.iterations() * long(bytes)));
+	state.SetLabel(std::to_string(bytes / 1024) + "kb");
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-	std::string x = "hello";
-	for (auto _ : state)
-		std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK(cacheBench)->DenseRange(13,26)->ReportAggregatesOnly(true);
 
 BENCHMARK_MAIN();
 
-//int main(int argc, char** argv)
-//{
-//	const RNG<int> cgen(0, 10, 2);
-//	for (int i : cgen)
-//	{
-//		std::cout << i << "\n";
-//	}
-//
-//	
-//	return 0;
-//}
