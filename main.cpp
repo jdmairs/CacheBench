4e2f19bb97fef75e9b374159532c10f4c5ae5b0e
John Mairs <John.D.Mairs@gmail.com>
2018-10-02 21:11:10 -0400

    got benchmark code to be found, linked, and run

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include "RNG.h"
+#include <benchmark/benchmark.h>
 
-
-int main(int argc, char** argv)
-{
-	const RNG<int> cgen(0, 10, 2);
-	for (int i : cgen)
-	{
-		std::cout << i << "\n";
-	}
-
-	
-	return 0;
+static void BM_StringCreation(benchmark::State& state) {
+	for (auto _ : state)
+		std::string empty_string;
 }
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+	std::string x = "hello";
+	for (auto _ : state)
+		std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+BENCHMARK_MAIN();
+
+//int main(int argc, char** argv)
+//{
+//	const RNG<int> cgen(0, 10, 2);
+//	for (int i : cgen)
+//	{
+//		std::cout << i << "\n";
+//	}
+//
+//	
+//	return 0;
+//}
